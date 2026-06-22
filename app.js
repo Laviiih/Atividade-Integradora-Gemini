80ec74ce9eeb7869b2a199948436a1afc2f54bb7
Lavinia <laviniaklaser@hotmail.com>
2026-06-22 17:18:29 -0300

    feat: implementa sistema de imc

---
+++ app.js
@@ -0,0 +1,34 @@
+var API_KEY = "AQ.Ab8RN6KbsW1nJMyqbhto3Sqb0LtiquNQP5PYMc5ctGLFs_ZJyw";
+
+function enviarRequisicao(){
+    var promptUsuario = document.getElementById("txtPrompt").value;
+
+    if (promptUsuario.trim() === "") {
+        alert("O campo de prompt não pode estar vazio!");
+        return;
+    }
+    document.getElementById("txtResposta").innerText = "Carregando Resposta... Aguarde um momento!";
+
+    var url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + API_KEY;
+    var payload = {
+        contents: [{
+            parts: [{
+                text: promptUsuario
+            }]
+        }]
+    }
+
+    fetch(url, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(payload)
+    })
+    .then(function(response) { return response.json(); })
+    .then (function(data){
+        var textoResposta = data.candidates[0].content.parts[0].text;
+        document.getElementById("txtResposta").innerText = textoResposta;
+    })
+    .catch(function(error){
+        document.getElementById("txtResposta").innerText = "Ocorreu um erro ao obter a resposta. Por favor, tente novamente."+ error;
+    }) 
+}
